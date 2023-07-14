427e6626c142b4cc99a2c7e641f944e380dd5fc8
donavonhayes <131027014+donavonhayes@users.noreply.github.com>
2023-07-13 19:59:57 -0500

    8 commit

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,14 +1,14 @@
-import "../App.css";
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Create from "../create/create";
-import Read from "../read/read";
-import Update from "../update/update";
+import Create from "./components/create/Create";
+import Read from "./components/read/Read";
+import Update from "./components/update/Update";
 
 function App() {
   return (
     <div className="main">
       <Routes>
-        <Route path="/create" element={<Create />} className="datagrid"/>
+        <Route path="/" element={<Create />} className="datagrid"/>
         <Route path="read" exact element={<Read />} className="datagrid"/>
         <Route path="update" exact element={<Update />} className="datagrid"/>
       </Routes>
