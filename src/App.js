3568c88565d7c7e76f535e27e74862dcb6fdbfea
donavonhayes <131027014+donavonhayes@users.noreply.github.com>
2023-07-13 19:33:06 -0500

    8 commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css'
+import Create from './components/create'
+import Read from './components/read'
+import Update from './components/update'
 import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/navbar'
+
+
 
 
 export default function App() {
@@ -8,9 +14,9 @@ export default function App() {
       <NavBar />
       <h2 className="main-header">React Crud Operations App</h2>
       <Routes>
-        <Route path="/" element={<Create />} />
-        <Route path="/read" element={<Read />} />
-        <Route path="/update" element={<Update />} />
+        <Route path="/Create" element={<Create />} />
+        <Route path="/Read" element={<Read />} />
+        <Route path="/Update" element={<Update />} />
       </Routes>
     </div>
   )
