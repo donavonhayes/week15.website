7f622695ecaaa237dbddbdfd08701dc4c0c8203c
donavonhayes <131027014+donavonhayes@users.noreply.github.com>
2023-07-17 10:10:27 -0500

    8 commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import './index.css';
-import App from './App';
+import App from '../App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
